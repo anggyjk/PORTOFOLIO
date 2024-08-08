bd418b37ea989b4f62779cf05d22d89a6427b1b4
anggyjk <anggyjogokurniawan@gmail.com>
2024-08-08 21:51:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+	strings:
+	["Student","Anime Lovers","Programmer","Dreamer","Gamer"],
+	typeSpeed: 100,
+	typeSpeed: 100,
+	backDelay: 1000,
+	loop: true
+});
